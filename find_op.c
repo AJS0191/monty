d79bb23f37f1d41f0201ab07d49fe9adb3f1d22f
AJS9091 <ashtonjamess@live.com>
2021-12-16 11:39:05 -0800

    adding find_op.c

---
+++ find_op.c
@@ -0,0 +1,17 @@
+void (find_op(char *possible_op))(stack_t **, unsigned int)
+{
+	instruction_t instructions[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if(strcmp(possible_op,instructions[i].opcode) == 0)
+		{
+			instructions[i].f;
+		}
+	}
+}
