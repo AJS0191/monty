4f12fa7beb95903e9a99d38888277ea9c11d03a3
CodeSlayer123 <3613@holbertonschool.com>
2021-12-16 12:08:44 -0800

    updated function names

--- push.c
+++ push.c
@@ -1,19 +1,27 @@
 /**
- * push - adds node to top of stack
+ * op_push - adds node to top of stack
+ * @n: data to be added to top of stack
  * @stack - the stack
  * @line_number: linenumber
- * @n: data to be added to top of stack
  * Return: none
 */
-void push(int n)
+void op_push(int n, int line_number)
 {
 	stack_t *newNode;
 	stack_t *head;
 
+	if (n == NULL || is(digit(n)) == 0)
+	{
+		fprintf(stderr, "%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	newNode = malloc(sizeof(stack_t));
 	if (newNode == NULL)
-		return (NULL);
-
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	newNode->n = number;
 	newNode->prev = NULL;
 	newNode->next = head;
