d870100499f9aac042dd1df88675f270ce7e5f00
AJS0191 <85968904+AJS0191@users.noreply.github.com>
2021-12-17 08:56:50 -0600

    Update collect_data.c
    update to work with pall and push

--- collect_data.c
+++ collect_data.c
@@ -2,59 +2,68 @@
 
 int collect_data(char *filename)
 {
-	FILE *fp;
-	char str1[10];
-	int number;
-	int c;
-	int wordcount = 0;
-	stack_t **stack = NULL;
-	unsigned int linecount = 1;
+        FILE *fp;
+        char str1[10];
+        int number;
+        int c;
+        int wordcount = 0;
+        int push = 0;
+        stack_t **stack = NULL;
+        unsigned int linecount = 0;
 
-	printf("FILENAME:%s\n", filename);
-	fp = fopen (filename, "r");
-	if (fp == NULL)
-	{
-		fprintf(stderr,"Error: Can't open file %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
-	while ((c = fgetc(fp)) != EOF)
-	{
-		ungetc(c, fp);
-		wordcount = 0;
-		while ((c = fgetc(fp)) != '\n')
-		{
-			if (wordcount >= 1)
-			{
-				while (fgetc(fp) != '\n')
+        printf("FILENAME:%s\n", filename);
+        fp = fopen (filename, "r");
+        if (fp == NULL)
+        {
+                fprintf(stderr,"Error: Can't open file %s\n", filename);
+                exit(EXIT_FAILURE);
+        }
+        while ((c = fgetc(fp)) != EOF)
+        {
+                ungetc(c, fp);
+                wordcount = 0;
+                while ((c = fgetc(fp)) != '\n')
+                {
+                        if (wordcount >= 1)
+                        {
+                                while (fgetc(fp) != '\n')
 					;
-				linecount++;
-				break;
-			}
-			ungetc(c, fp);
-			fscanf(fp, "%s", str1);
-			if (strcmp(str1, "push"))
-			{
-				fscanf(fp, "%d", &number);
-				wordcount++;
-			}
-			wordcount++;
-			if (stack == NULL)
-			{
-				if (strcmp(str1, "push") == 0)
-				{
-					stack = stack_builder(number, 100);
-					break;
-				}
-				else
-				{
-					fprintf(stderr, "Error: malloc failed\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-			find_op(str1)(stack, linecount);
-		}
+                                break;
+                        }
+                        linecount++;
+                        ungetc(c, fp);
+                        fscanf(fp, "%s", str1);
+                        printf("Got the command: %s\n", str1);
+                        if (strcmp(str1, "push") == 0)
+                        {
+                                fscanf(fp, "%d", &number);
+                                printf("Got the value: %d\n", number);
+                                wordcount++;
+                                push = 1;
+                        }
+                        wordcount++;
+                        if (stack == NULL)
+                        {
+                                push = 0;
+                                if (strcmp(str1, "push") == 0)
+                                {
+                                        stack = stack_builder(number, 100);
+                                }
+                                else
+                                {
+                                        fprintf(stderr, "Error: malloc failed\n");
+                                        exit(EXIT_FAILURE);
+                                }
+                        }
+                        printf("Line_Count is %d\n", linecount);
+			if (push == 1)
+                                (*stack)->n = number;
+                        printf("*stack->n: %d\n", (*stack)->n);
+                        find_op(str1)(stack, linecount);
+                        push = 0;
+                }
 
-	}
-	fclose(fp);
-	return (0);
+        }
+        fclose(fp);
+        return (0);
 }
