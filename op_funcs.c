347d813ba3a55a9b7ee549b1fbedcb71e240bc35
AJS9091 <ashtonjamess@live.com>
2021-12-18 11:19:37 -0800

    fixing free (stack)

--- op_funcs.c
+++ op_funcs.c
@@ -33,7 +33,6 @@ void op_pint(stack_t **stack, unsigned int line_number)
 	if (stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
