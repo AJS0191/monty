cd1544a4ed28f4bf5548d33888f639f829db479c
AJS9091 <ashtonjamess@live.com>
2021-12-18 09:50:56 -0800

    swap

--- op_funcs.c
+++ op_funcs.c
@@ -85,7 +85,7 @@ void op_swap(stack_t **stack, unsigned int line_number)
 
 	if (!stack)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*stack);
